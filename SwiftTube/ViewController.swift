f1b340ad1a596f74c6497fa3a2ee3cf1d1ae6c03
Pascal Raszyk <pr@mediatrooper.de>
2014-11-16 18:47:17 +0100

    Initial Commit

---
+++ SwiftTube/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftTube
+//
+//  Created by Pascal Raszyk on 16.11.14.
+//  Copyright (c) 2014 Pascal Raszyk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
